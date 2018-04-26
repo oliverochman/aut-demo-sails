85bd96b858a9e25e76734af12b7309c430831b26
oliver_ochman <oliver.ochman@hotmail.com>
2018-04-26 15:33:17 +0200

    change feature file update h to make tests go green

--- features/step_definitions/basic_steps.js
+++ features/step_definitions/basic_steps.js
@@ -1,4 +1,4 @@
-const { After, Given, Then } = require("cucumber");
+const { After, Given, Then, When } = require("cucumber");
 
 After(async function() {
   return await this.closeHomePage();
@@ -11,3 +11,23 @@ Given("I visit the site", async function() {
 Then("I should see {string}", async function(content) {
   return await this.pageHasTextContent(content);
 });
+
+When('I click {string}', async function(string) {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending'
+})
+
+Then('I fill in {string} with {string}', async function(string, string2) {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending'
+})
+
+Then('I should have {int} contact in my address book', async function(int) {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending'
+})
+
+Then('I should not see {string}', async function(string) {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending'
+})
