e1e7c6e9d5d961a7cbd72ccfd1c4005a046e7fa5
oliver_ochman <oliver.ochman@hotmail.com>
2018-04-26 16:54:07 +0200

    WiP create contact model and controller

---
+++ api/models/Contact.js
@@ -0,0 +1,10 @@
+module.exports = {
+  attributes: {
+    name : { type: 'text' },
+    email : { type: 'string' },
+    phone : { type: 'text' },
+    company : { type: 'text' },
+    notes : { type: 'text' },
+    twitter : { type: 'text' }
+  }
+};
