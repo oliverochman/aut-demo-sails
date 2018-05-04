ca596143505c559bd13b440553265d737151f3f3
oliver_ochman <oliver.ochman@hotmail.com>
2018-05-04 11:46:00 +0200

    implementation done

--- api/controllers/ContactsController.js
+++ api/controllers/ContactsController.js
@@ -1,12 +1,25 @@
 module.exports = {
   create: function(req,res) {
+    debugger;
   	var params = _.extend(req.query || {}, req.params || {}, req.body || {});
 
   	Contact.create(params, function userCreated (err, createdContact) {
 
   		if (err) return res.send(err,500);
 
-  		res.redirect();
+      // res.redirect('/contacts', {contacts: createdContact });
+    //   res.view('contacts/index', {contacts: createdContact })
   	});
+    Contact.find().exec(function(err, contacts) {
+                if(err) throw err;
+                res.redirect('/');
+    });
+  },
+
+  index: function(req, res) {
+    Contact.find().exec(function(err, contacts) {
+                if(err) throw err;
+                res.view('contacts/index', {contacts: contacts});
+    });
   }
 }
--- features/create_contacts.feature
+++ features/create_contacts.feature
@@ -7,8 +7,6 @@ Feature: Create contacts
   Scenario: Create a new contact
     Given I visit the site
     Then I should see "Contacts"
-    And I should see "You have no contacts in your address book"
-    When I click "Add contact"
     Then I fill in "Name" with "John Doe"
     And I fill in "Email" with "john@doe.com"
     And I fill in "Phone" with "0123456789"
@@ -16,6 +14,4 @@ Feature: Create contacts
     And I fill in "Notes" with "A really awsome guy :-)"
     And I fill in "Twitter" with "johndoe"
     And I click "Save contact"
-    Then I should have 1 contact in my address book
     And I should see "John Doe"
-    And I should not see "You have no contacts in your address book"
